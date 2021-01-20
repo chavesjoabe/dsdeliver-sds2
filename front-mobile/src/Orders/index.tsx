2bd6b2ba5d59b1e2dabe53eb8032d2a926eb91ba
chavesjoabe <chavesjoabe1@gmail.com>
2021-01-19 23:24:39 -0300

    add orders list

---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ScrollView, StyleSheet, Text, View,  } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
+import OrderCard from '../OrderCard';
+
+function Orders() {
+
+    const handleOnPress = ()=>{
+
+    }
+    return (
+        <>
+            <Header />
+            <ScrollView style={styles.container}>
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+            </ScrollView>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({ container:{
+    paddingRight: '5%',
+    paddingLeft: '5%',
+ }
+});
+
+export default Orders;
